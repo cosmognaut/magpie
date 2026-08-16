409ac8c7bc64bbe2a2eb30af3c637bced864c60d
cosmog <ishuman2006@gmail.com>
2026-08-16 23:56:17 +0530

    feat: implement duration overlay; details later

--- frontend/src/lib/Genre.svelte
+++ frontend/src/lib/Genre.svelte
@@ -8,7 +8,6 @@
 	const scrollValue: number = 600;
 	// element is a reference to a DOM node which contains the element on which we want to scroll
 	function scrollRight(){
-		console.log(element.scrollLeft);
 		element.scrollBy({
 		  top: 0,
 		  left: scrollValue,
@@ -47,7 +46,7 @@
 	<h2 class="font-medium text-4xl px-4 font-display">{name}</h2>
 	<p class="px-4 font-mono">This is some text related to the genre..</p>
 	{#if scrolled}
-		<button class="absolute top-[55%] left-1 bg-emerald-700 rounded-none p-3 hover:bg-emerald-800 cursor-pointer border rotate-180" aria-label="Scroll" onclick={scrollLeft} transition:fade>
+		<button class="absolute top-[55%] left-1 bg-emerald-700 rounded-none p-3 hover:bg-emerald-800 cursor-pointer border rotate-180 z-10" aria-label="Scroll" onclick={scrollLeft} transition:fade>
 			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fef3c6" d="M16.15 13H5q-.425 0-.712-.288T4 12t.288-.712T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L19.3 11.3q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7z"/></svg>
 		</button>
 	{/if}
--- frontend/src/lib/Video.svelte
+++ frontend/src/lib/Video.svelte
@@ -1,10 +1,11 @@
-<div class="border w-110 h-75 flex-none py-2 px-2">
-	<a href="https://www.youtube.com/watch?v=yxSd-lcmbGY">
+<div class="border w-110 h-75 flex-none py-2 px-2 relative">
+	<!-- <a href="https://www.youtube.com/watch?v=yxSd-lcmbGY"> -->
 		<img src="https://i.ytimg.com/vi/-JAFb2bYJSs/maxresdefault.jpg" alt="video thumbnail" class="w-full h-4/5">
+		<p class="bg-black absolute bottom-15 right-2 p-1">1:12:02</p>
 		<p class="mt-1 font-mono font-semibold line-clamp-1">China destroyed its tech giants. Here's why.</p>
 		<div class="video-info flex flex-row justify-between">
 			<p class="mb-1 font-mono font-light">TechAltar</p>
 			<p class="font-mono font-light">1M views</p>
 		</div>
-	</a>
+	<!-- </a> -->
 </div>
