233ad3fc8a6e8f756bbe71db90ee59083dc1bcbe
cosmog <ishuman2006@gmail.com>
2026-08-16 19:35:38 +0530

    feat: show binding pattern for child to parent data flow
    now choose one. Either binding or callback. Personally, I think that
    callback is cleaner here, and also because $bindable should be used
    sparingly and with caution.

--- frontend/src/lib/Search.svelte
+++ frontend/src/lib/Search.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	let { onInput } = $props();
+	let { value=$bindable() } = $props();
 </script>
 
 <div class="mr-4 ml-4">
-	<input type="text" placeholder="Search genres..." class="border p-4 mt-10 mb-2 px-4 focus:outline-none text-xl w-full focus:border-emerald-500" oninput={onInput}/>
+	<input type="text" placeholder="Search genres..." class="border p-4 mt-10 mb-2 px-4 focus:outline-none text-xl w-full focus:border-emerald-500"  bind:value={value}/>
 </div>
