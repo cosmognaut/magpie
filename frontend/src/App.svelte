3a31df1c826d0075723f055950b8a7281f4839f8
cosmog <ishuman2006@gmail.com>
2026-08-23 00:44:18 +0530

    fix: make it so that the pathname for home is '', for ex. magpie.ishu.foo and not magpie.ishu.foo/home

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -13,7 +13,7 @@
 
 	function updatePage(event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement}, name: string) {
 		event.preventDefault();
-		history.pushState(null, "", name); // the last param controls what's displayed after the / in the URL.
+		if (name !== 'home') history.pushState(null, "", name); // the last param controls what's displayed after the / in the URL. I prefer there not being a magpie.ishu.foo/home page, but rather only magpie.ishu.foo, that's the reason for this conditional.
 		page = name;
 	}
 
